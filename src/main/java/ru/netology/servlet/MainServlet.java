f182bd669077651ddfcc96d2095bd43501b7a39d
Mikhail Kasimov <nsmikawork@gmail.com>
2024-07-23 20:23:06 +0300

    Edit MainServlet

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -11,6 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 public class MainServlet extends HttpServlet {
     private PostController controller;
 
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private static final String API_POSTS = "/api/posts";
+    private static final String API_POSTS_REGEX = "/api/posts/\\d+";
+
     @Override
     public void init() {
         final var repository = new PostRepository();
@@ -24,20 +30,20 @@ public class MainServlet extends HttpServlet {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
             // primitive routing
-            if (method.equals("GET") && path.equals("/api/posts")) {
+            if (GET.equals(method) && API_POSTS.equals(path)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+            if (GET.equals(method) && path.matches(API_POSTS_REGEX)) {
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (POST.equals(method) && API_POSTS.equals(path)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+            if (DELETE.equals(method) && path.matches(API_POSTS_REGEX)) {
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.removeById(id, resp);
                 return;
